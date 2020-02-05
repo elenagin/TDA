26b45007d096df4c80db4be53561010d4b32dc21
lennygin <lennginzan@gmail.com>
2020-02-05 11:05:11 -0600

    Finishing code

--- TDA/exDataTypes/data_type.h
+++ TDA/exDataTypes/data_type.h
@@ -7,26 +7,22 @@ enum data_type
     Char = 2
 };
 
-void data_type()
-{
-    char input[SIZE];
-    int longitud, num;
+int data_type(char input[SIZE])
+{   
+    int num = 0, num2 = 0;
 
-    fgets(input, 20, stdin);
-    longitud = strlen(input);
-    if ((longitud > 0) && (input[longitud - 1] == '\n'))
+    for (int i = 0; i < strlen(input); i++)
     {
-        input[longitud - 1] = 0;
+        if (input[i] < 48 || input[i] > 57)
+            num++;
+        if (input[i] == 46)
+            num2++;
     }
 
-    if (atoi(input) == 0 && strchr(input, ' ') == NULL)
-        printf("Char %d\n", Char);
-
-    else if (atof(input) != 0 && strchr(input, '.') != NULL)
-        printf("Float %d\n", Float);
-
-    else if (atoi(input) != 0)
-        printf("Int %d\n", Int);
-
-
+    if (num == 0)
+        return Int;
+    else if (num == 1 && num2 == 1)
+        return Float;
+    else
+        return Char;
 }
--- TDA/exDataTypes/main.c
+++ TDA/exDataTypes/main.c
@@ -4,10 +4,27 @@
 
 #include "data_type.h"
 
+
 int main()
 {
     system("clear");
-    data_type();
+
+    char input[50];
+    int length;
+
+    fgets(input, 20, stdin);
+    length = strlen(input);
+    if ((length > 0) && (input[length - 1] == '\n'))
+    {
+        input[length - 1] = 0;
+    }
+    if (data_type(input) == Int)
+        printf("Input is INT");
+    else if (data_type(input) == Float)
+        printf("Input is FLOAT");
+    else
+        printf("Input is CHAR");
+
     getchar();
     system("clear");
     return 0;
